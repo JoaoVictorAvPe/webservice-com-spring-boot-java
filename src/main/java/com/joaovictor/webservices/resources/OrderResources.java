db6f922004e8a67f6c3783952afef8ed5348518c
Joao Victor <joaoavilaperasol@hotmail.com>
2026-04-16 20:35:10 -0300

    created entity order and associations

---
+++ src/main/java/com/joaovictor/webservices/resources/OrderResources.java
@@ -0,0 +1,33 @@
+package com.joaovictor.webservices.resources;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.joaovictor.webservices.entities.Order;
+import com.joaovictor.webservices.services.OrderService;
+
+@RestController
+@RequestMapping(value = "/orders")
+public class OrderResources {
+    
+    @Autowired
+    private OrderService service;
+
+    @GetMapping
+    public ResponseEntity<List<Order>> findAll() {
+        List<Order> orders = service.findAll();
+        return ResponseEntity.ok().body(orders);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Order> findById(@PathVariable Long id) {
+        Order order = service.findById(id);
+        return ResponseEntity.ok().body(order);
+    }
+}

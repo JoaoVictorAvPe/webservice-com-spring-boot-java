f044f797f435fb3fe53b94c5bce79e52008f6063
Joao Victor <joaoavilaperasol@hotmail.com>
2026-04-22 20:27:19 -0300

    OrderItem and Product association

--- src/main/java/com/joaovictor/webservices/entities/Product.java
+++ src/main/java/com/joaovictor/webservices/entities/Product.java
@@ -4,10 +4,13 @@ import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Set;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -29,6 +32,9 @@ public class Product implements Serializable{
         joinColumns = @jakarta.persistence.JoinColumn(name = "product_id"),
         inverseJoinColumns = @jakarta.persistence.JoinColumn(name = "category_id"))
     private Set<Category> categories = new HashSet<>();
+
+    @OneToMany(mappedBy = "id.product")
+    private Set<OrderItem> items = new HashSet<>();
 
     public Product() {
     }
@@ -83,6 +89,15 @@ public class Product implements Serializable{
 
     public Set<Category> getCategories() {
         return categories;
+    }
+
+    @JsonIgnore
+    public Set<Order> getOrders() {
+        Set<Order> set = new HashSet<>();
+        for (OrderItem x : items) {
+            set.add(x.getOrder());
+        }
+        return set;
     }
 
     @Override
